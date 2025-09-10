f57fc857807b56cc67cb8b8f42eb7ac5dedc3563
Cameron Smith <cameron.smith3030@gmail.com>
2025-09-10 18:02:25 -0400

    function to return array of booleans to move mole around randomly

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { useRef } from "react";
+
+function makeMole() {
+  const holes = Array.from({ length: 9 }, () => false);
+
+  const randomIndex = Math.floor(Math.random() * 9);
+
+  holes[randomIndex] = true;
+
+  return holes;
+}
+
 export default function App() {
+  const mole = useRef(makeMole());
   return <></>;
 }
