aafe28db56bc9b92f0ec15d73025a0df453b7cc2
Cameron Smith <cameron.smith3030@gmail.com>
2025-09-17 18:11:15 -0400

    css and class names

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,12 @@ export default function Welcome() {
   const { start, isGame } = useGame();
   return (
     <>
-      <h2>Whack-A-Mole</h2>
+      <h2>Welcome to the best Whack-A-Mole Game</h2>
+      <p>
+        Catch these pesky moles by clicking on them! Be careful though, they
+        move around a lot!
+      </p>
+      <aside>Press START to play!</aside>
       <button id="start" aria-label="start game" onClick={start}>
         {isGame ? "End" : "Start"}
       </button>
